8a65db1c8f181f5b1a1c348e1a920c3efbd69f11
Pramod Kumar Rapolu <pramod_kumar_rapolu@bose.com>
2020-05-19 18:29:50 -0400

    Added in an empty ProfileCreationActivity and its layout. Also implemented the common layout/view between creation and confirmation screen as part of separate "header_layout" as the first step so that it can re used.

---
+++ app/src/main/java/com/mobile/profilecreator/ProfileCreationActivity.kt
@@ -0,0 +1,12 @@
+package com.mobile.profilecreator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileCreationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_creation)
+    }
+}
